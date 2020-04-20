12710c1c8fe588c80f03a7e00a063da2a60bc7a6
Stanislav Kutasevits <stan@bellcom.dk>
2020-04-20 10:57:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# OS2Web Meetings SBSYS Drupal module
+
+## Module purpose
+
+The aim of this module is to import meetings from SBSYS ESDH providers.
+
+This modules is highly dependent on  [Os2web Meetings](https://github.com/OS2web/os2web_meetings) module and serves as an implementation of it.
+
+## How does it work
+
+Meetings are being imported from so called agenda or manifest files, that are provided by specific SBSYS ESDH provider.
+
+OS2Web Meetings SBSYS serves as an adapter that converts meetings into a canonical format.
+
+After "translating" each meeting is then processed and eventually imported in the system.
+
+Import is handled via Migrate API, which is part of the Drupal 8 core functionality.
+
+## Additional settings
+Settings are available under ```admin/config/content/os2web-meetings```
+* **SBSYS Meetings manifests path** - Path to agenda directory.
+
+## Install
+
+Module is available to download via composer.
+```
+composer require os2web/os2web_meetings_sbsys
+drush en os2web_meetings_sbsys
+```
+
+## Import process
+
+The import process can be done in two ways:
+* Via Drush (recommended)
+    * Use the following Drush command to start the migration:
+        ```
+        drush migrate:import os2web_meetings_sbsys_import
+        ```
+        Read more about the Drush commands for Migrate API on [Migrate tools](https://www.drupal.org/project/migrate_tool).
+    * It is highly recommended to set up a cronjob on your server to do the run this command often
+
+* Via Admin UI
+    * Go to ```admin/structure/migrate/manage/os2web_meetings/migrations``` on your installation
+    * Click ```Execute``` next to **Meeting import (SBSYS)**
+    * Click ```Execute``` on the next page as well (doing that will use default options).
+
+## Update
+Updating process for OS2Web Meetings module is similar to usual Drupal 8 module.
+Use Composer's built-in command for listing packages that have updates available:
+
+```
+composer outdated os2web/os2web_meetings_sbsys
+```
+
+## Automated testing and code quality
+See [OS2Web testing and CI information](https://github.com/OS2Web/docs#testing-and-ci)
+
+## Contribution
+
+Project is opened for new features and os course bugfixes.
+If you have any suggestion or you found a bug in project, you are very welcome
+to create an issue in github repository issue tracker.
+For issue description there is expected that you will provide clear and
+sufficient information about your feature request or bug report.
+
+### Code review policy
+See [OS2Web code review policy](https://github.com/OS2Web/docs#code-review)
+
+### Git name convention
+See [OS2Web git name convention](https://github.com/OS2Web/docs#git-guideline)
---
+++ src/Plugin/migrate/source/MeetingsDirectorySbsys.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace Drupal\os2web_meetings_sbsys\Plugin\migrate\source;
+
+use Drupal\migrate\Event\MigrateImportEvent;
+use Drupal\node\Entity\Node;
+use Drupal\os2web_meetings\Entity\Meeting;
+use Drupal\os2web_meetings\Form\SettingsForm;
+use Drupal\os2web_meetings\Plugin\migrate\source\MeetingsDirectory;
+
+/**
+ * Source plugin for retrieving data via URLs.
+ *
+ * @MigrateSource(
+ *   id = "os2web_meetings_directory_sbsys"
+ * )
+ */
+class MeetingsDirectorySbsys extends MeetingsDirectory {
+  /**
+   * {@inheritdoc}
+   */
+  public function getMeetingsManifestPath() {
+    return \Drupal::config(SettingsForm::$configName)
+      ->get('sbsys_meetings_manifest_path');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaAccessToCanonical(array $source) {
+    if (strcasecmp($source['agenda_access'], 'true') === 0) {
+      return MeetingsDirectory::AGENDA_ACCESS_OPEN;
+    }
+    else {
+      return MeetingsDirectory::AGENDA_ACCESS_CLOSED;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaTypeToCanonical(array $source) {
+    if (strcasecmp($source['agenda_type'], 'referat') === 0) {
+      return MeetingsDirectory::AGENDA_TYPE_REFERAT;
+    }
+    else {
+      return MeetingsDirectory::AGENDA_TYPE_DAGSORDEN;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertStartDateToCanonical(array $source) {
+    $start_date = $source['meeting_start_date'] . ' ' . $source['meeting_start_time'];
+
+    return $this->convertDateToTimestamp($start_date);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertEndDateToCanonical(array $source) {
+    $end_date = $source['meeting_end_date'] . ' ' . $source['meeting_end_time'];
+
+    return $this->convertDateToTimestamp($end_date);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaDocumentToCanonical(array $source) {
+    $title = 'Samlet document';
+    // There is no reference to HTML file, but we expect it to be in the
+    // directory with the following name.
+    $uri = 'Dagsorden.html';
+
+    return [
+      'title' => $title,
+      'uri' => $uri,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertCommitteeToCanonical(array $source) {
+    $id = $source['committee_id'];
+    $name = $source['committee_name'];
+
+    return [
+      'id' => $id,
+      'name' => $name,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertLocationToCanonical(array $source) {
+    $name = $source['location_name'];
+    // We don't have an ID for the location, use name instead.
+    $id = $name;
+
+    return [
+      'id' => $id,
+      'name' => $name,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertBulletPointsToCanonical(array $source) {
+    $canonical_bullet_points = [];
+    $source_bullet_points = $source['bullet_points'];
+
+    foreach ($source_bullet_points as $bullet_point) {
+      $id = $bullet_point['@attributes']['DagsordenpunktID'];
+      $title = $bullet_point['Overskrift'];
+      $access = filter_var($bullet_point['@attributes']['Åbent'], FILTER_VALIDATE_BOOLEAN);
+
+      // Getting attachments (text).
+      $source_attachments = $bullet_point['Indhold'];
+      $canonical_attachments = [];
+      if (is_array($source_attachments)) {
+        // Handling single items.
+        if (array_key_exists('@attributes', $source_attachments)) {
+          $source_attachments = [$source_attachments];
+        }
+
+        $canonical_attachments = $this->convertAttachmentsToCanonical($source_attachments);
+      }
+
+      // Getting enclosures (files).
+      $source_enclosures = $bullet_point['Bilagsliste']['Bilag'];
+      $canonical_enclosures = [];
+      if (is_array($source_enclosures)) {
+        // Handling single items.
+        if (array_key_exists('@attributes', $source_enclosures)) {
+          $source_enclosures = [$source_enclosures];
+        }
+
+        $canonical_enclosures = $this->convertEnclosuresToCanonical($source_enclosures);
+      }
+
+      // Merging enclosures into attachments.
+      $canonical_attachments = array_merge($canonical_attachments, $canonical_enclosures);
+
+      $canonical_bullet_points[] = [
+        'id' => $id,
+        'title' => $title,
+        'access' => $access,
+        'attachments' => $canonical_attachments,
+      ];
+    }
+
+    return $canonical_bullet_points;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAttachmentsToCanonical(array $source_attachments) {
+    $canonical_attachments = [];
+
+    foreach ($source_attachments as $title => $body) {
+      // Using title as ID, as we don't have a real one.
+      $id = $title;
+
+      $canonical_attachments[] = [
+        'id' => $id,
+        'title' => $title,
+        'body' => $body,
+        'access' => TRUE,
+      ];
+    }
+
+    return $canonical_attachments;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertEnclosuresToCanonical(array $source_enclosures) {
+    $canonical_enclosures = [];
+
+    foreach ($source_enclosures as $enclosure) {
+      $id = $enclosure['@attributes']['BilagID'];
+      $title = $enclosure['@attributes']['Navn'];
+      $access = filter_var($enclosure['@attributes']['MaaPubliceres'], FILTER_VALIDATE_BOOLEAN);
+      $uri = $enclosure['@attributes']['BilagUrl'];
+
+      $canonical_enclosures[] = [
+        'id' => $id,
+        'title' => $title,
+        'uri' => $uri,
+        'access' => $access,
+      ];
+    }
+
+    return $canonical_enclosures;
+  }
+
+  /**
+   * Converts Danish specific string date into timestamp in UTC.
+   *
+   * @param string $dateStr
+   *   Date as string, e.g. "27. august 2018 16:00".
+   *
+   * @return int
+   *   Timestamp in UTC.
+   *
+   * @throws \Exception
+   */
+  private function convertDateToTimestamp($dateStr) {
+    $dateStr = str_ireplace([
+      ". januar ",
+      ". februar ",
+      ". marts ",
+      ". april ",
+      ". maj ",
+      ". juni ",
+      ". juli ",
+      ". august ",
+      ". september ",
+      ". oktober ",
+      ". november ",
+      ". december ",
+    ],
+      [
+        "-1-",
+        "-2-",
+        "-3-",
+        "-4-",
+        "-5-",
+        "-6-",
+        "-7-",
+        "-8-",
+        "-9-",
+        "-10-",
+        "-11-",
+        "-12-",
+      ], $dateStr);
+
+    $dateTime = new \DateTime($dateStr, new \DateTimeZone('Europe/Copenhagen'));
+
+    return $dateTime->getTimestamp();
+  }
+
+  public function postImport(MigrateImportEvent $event) {
+    parent::postImport($event);
+
+    // Find all meetings.
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'os2web_meetings_meeting');
+    $query->condition('field_os2web_m_source', $this->getPluginId());
+    $entity_ids = $query->execute();
+
+    $meetings = Node::loadMultiple($entity_ids);
+
+    // Group meetings as:
+    // $groupedMeetings[<meeting_id>][<agenda_id>] = <node_id> .
+    $groupedMeetings = [];
+    foreach ($meetings as $meeting) {
+      $os2webMeeting = new Meeting($meeting);
+
+      $meeting_id = $os2webMeeting->getMeetingId();
+      $agenda_id = $os2webMeeting->getEsdhId();
+
+      $groupedMeetings[$meeting_id][$agenda_id] = $os2webMeeting->id();
+
+      // Sorting agendas, so that lowest agenda ID is always the first.
+      sort($groupedMeetings[$meeting_id]);
+    }
+
+    // Process grouped meetings and set addendum fields.
+    foreach ($groupedMeetings as $meeting_id => $agendas) {
+      // Skipping if agenda count is 1.
+      if (count($agendas) == 1) {
+        continue;
+      }
+
+      $mainAgendaNodedId = array_shift($agendas);
+
+      foreach ($agendas as $agenda_id => $node_id) {
+        // Getting the meeting.
+        $os2webMeeting = new Meeting($meetings[$node_id]);
+
+        // Setting addendum field, meeting is saved inside a function.
+        $os2webMeeting->setAddendum($mainAgendaNodedId);
+      }
+    }
+  }
+
+}
